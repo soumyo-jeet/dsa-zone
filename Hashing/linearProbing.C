092399b046a2f0c232c6131e2389264ec32910be
soumyo-jeet <samajdarsoumyajeet0@gmail.com>
2025-12-04 23:32:07 +0530

    update

--- Hashing/linearProbing.C
+++ Hashing/linearProbing.C
@@ -1,82 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct HashTable {
+typedef struct Pair
+{
+    char *key, *value;
+} pair;
+
+typedef struct HashTable
+{
     int size;
-    int *arr;
     int elec;
+    pair **arr;
 } hashTable;
 
-hashTable *initHashTable (int s)
+hashTable *initHashTable(int s)
 {
-    hashTable *ht = (hashTable *) malloc(sizeof(hashTable));
+    hashTable *ht = (hashTable *)malloc(sizeof(hashTable));
     ht->size = s;
-    ht->arr = (int *) malloc(sizeof(int) * s);
+    ht->arr = (pair **)malloc(sizeof(pair *) * s);
     ht->elec = 0;
 
-    for(int i = 0; i < s; i++)
+    for (int i = 0; i < s; i++)
     {
-        ht->arr[i] = 0;
+        ht->arr[i] = NULL;
     }
 
     return ht;
 }
 
-void insert (hashTable *ht, int x, int s)
+pair *createPair(char *key, char *value)
 {
-    if(ht->elec == s) 
+    pair *p = (pair *)malloc(sizeof(pair));
+    p->key = strdup(key);
+    p->value = strdup(value);
+
+    return p;
+}
+
+int hash(char *s, hashTable *ht)
+{
+    int c = 0;
+    for (int i = 0; i < strlen(s); i++)
     {
-        printf("Element count exceeded!");
-        return;
+        c += s[i];
     }
 
-    int id = x % ht->size;
-    while(ht->arr[id] != 0)
+    return (c % ht->size);
+}
+
+void insert(hashTable *ht, char *key, char *value)
+{
+    int id = hash(key, ht);
+    while (ht->arr[id] != NULL)
     {
         id = (id + 1) % ht->size;
     }
 
-    ht->arr[id] = x;
-    ht->elec ++;
+    ht->arr[id] = createPair(key, value);
+    ht->elec++;
 }
 
-void display (hashTable *ht)
+void display(hashTable *ht)
 {
     printf("\n Display of the hashtable: \n");
-    for(int i = 0; i < ht->size; i++)
+    for (int i = 0; i < ht->size; i++)
     {
-        printf("%d ", ht->arr[i]);
+        if (ht->arr[i] == NULL)
+            continue;
+        printf("%s : %s \n", ht->arr[i]->key, ht->arr[i]->value);
     }
 }
 
-int search (hashTable *ht, int x)
+int search(hashTable *ht, char *key)
 {
-    int id = x % ht->size, c = 0;
-    while(ht->arr[id] != x && ht->arr[id] != 0 && c != ht->elec)
+    int id = hash(key, ht), c = 0;
+    while (ht->arr[id] != NULL && strcmp(ht->arr[id]->key, key) != 0)
     {
         id = (id + 1) % ht->size;
-        
     }
 
-    if(ht->arr[id] != x) return -1;
+    if (ht->arr[id] == NULL || strcmp(ht->arr[id]->key, key) != 0)
+        return -1;
     return id;
 }
 
-int remove (hashTable *ht, int x)
+int remove(hashTable *ht, char *key)
 {
-    int id = x % ht->size, c = 0;
-    while(ht->arr[id] != x && ht->arr[id] != 0 && c != ht->elec)
+    int id = hash(key, ht), c = 0;
+    while (ht->arr[id] != NULL && strcmp(ht->arr[id]->key, key) != 0)
     {
         id = (id + 1) % ht->size;
-        
     }
 
-    if(ht->arr[id] != x) return 0;
-    ht->arr[id] = 0;
-    ht->elec --;
+    if (ht->arr[id] == NULL || strcmp(ht->arr[id]->key, key) != 0)
+        return 0;
+
+    ht->arr[id] = NULL;
+    ht->elec--;
     return 1;
 }
-
 
 int main()
 {
@@ -85,51 +108,60 @@ int main()
     scanf("%d", &s);
     hashTable *ht = initHashTable(s);
 
-    int as;
-    printf("Number of element want to put: ");
-    scanf("%d", &as);
+    char *key = (char *) malloc(sizeof(char)*50), *value = (char *) malloc(sizeof(char)*50);
 
     int f;
     printf("1. Insert, 2. Search, 3. Display, 4. Delete: ");
     scanf("%d", &f);
-    while(f != 5)
+    while (f != 5)
     {
-        if(f==1)
+        if (f == 1)
         {
-            int x;
-            printf("Element: ");
-            scanf("%d", &x);
-            insert(ht,x,as);
+            printf("Key: ");
+            scanf("%s", key);
+            printf("Value: ");
+            scanf("%s", value);
+            printf("Got %s : %s\n", key, value);
+
+            insert(ht, key, value);
         }
 
-        else if(f == 2)
+        else if (f == 2)
         {
-            int x;
-            printf("Element: ");
-            scanf("%d", &x);
-            int r = search(ht, x);
-            printf("Present at: %d\n", r);
+            printf("Key: ");
+            scanf("%s", key);
+            int r = search(ht, key);
+            char ans[100];
+            if (r != -1)
+                strcpy(ans, ht->arr[r]->value);
+            else
+                strcpy(ans, "Not Found!");
+
+            printf("Value: %s\n", ans);
         }
 
-        else if(f == 3)
+        else if (f == 3)
         {
             display(ht);
         }
 
-        else if(f==4)
+        else if (f == 4)
         {
-            int x;
-            printf("Element: ");
-            scanf("%d", &x);
-            int r = remove(ht, x);
-            if(r == 1) printf("Deleted succesfully!");
+            printf("Key: ");
+            scanf("%s", key);
+            int r = remove(ht, key);
+            if (r == 1)
+                printf("Deleted succesfully!");
         }
-        else break;
+        else
+            break;
 
         printf("1. Insert, 2. Search, 3. Display, 4. Delete: ");
         scanf("%d", &f);
     }
-    
+
     free(ht->arr);
     free(ht);
+    free(key);
+    free(value);
 }
